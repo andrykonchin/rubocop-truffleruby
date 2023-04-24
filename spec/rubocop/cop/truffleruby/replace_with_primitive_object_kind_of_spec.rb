a25b6da017246d981d550791faa9b53ab058805b
Andrew Konchin <andry.konchin@gmail.com>
2023-04-24 19:55:17 +0300

    Use new Primitive names

--- spec/rubocop/cop/truffleruby/replace_with_primitive_object_kind_of_spec.rb
+++ spec/rubocop/cop/truffleruby/replace_with_primitive_object_kind_of_spec.rb
@@ -6,66 +6,66 @@ RSpec.describe RuboCop::Cop::TruffleRuby::ReplaceWithPrimitiveObjectKindOf, :con
   it 'registers an offense when using `#kind_of?`' do
     expect_offense(<<~RUBY)
       a.kind_of?(String)
-      ^^^^^^^^^^^^^^^^^^ TruffleRuby/ReplaceWithPrimitiveObjectKindOf: Use `Primitive.object_kind_of?` instead of `#kind_of?` or `#is_a?`
+      ^^^^^^^^^^^^^^^^^^ TruffleRuby/ReplaceWithPrimitiveObjectKindOf: Use `Primitive.is_a?` instead of `#kind_of?` or `#is_a?`
     RUBY
 
     expect_correction(<<~RUBY)
-      Primitive.object_kind_of?(a, String)
+      Primitive.is_a?(a, String)
     RUBY
   end
 
   it 'registers an offense when using `#is_a?`' do
     expect_offense(<<~RUBY)
       a.is_a?(String)
-      ^^^^^^^^^^^^^^^ TruffleRuby/ReplaceWithPrimitiveObjectKindOf: Use `Primitive.object_kind_of?` instead of `#kind_of?` or `#is_a?`
+      ^^^^^^^^^^^^^^^ TruffleRuby/ReplaceWithPrimitiveObjectKindOf: Use `Primitive.is_a?` instead of `#kind_of?` or `#is_a?`
     RUBY
 
     expect_correction(<<~RUBY)
-      Primitive.object_kind_of?(a, String)
+      Primitive.is_a?(a, String)
     RUBY
   end
 
   it 'registers an offense when using `Module#===`' do
     expect_offense(<<~RUBY)
       String === a
-      ^^^^^^^^^^^^ TruffleRuby/ReplaceWithPrimitiveObjectKindOf: Use `Primitive.object_kind_of?` instead of `#kind_of?` or `#is_a?`
+      ^^^^^^^^^^^^ TruffleRuby/ReplaceWithPrimitiveObjectKindOf: Use `Primitive.is_a?` instead of `#kind_of?` or `#is_a?`
     RUBY
 
     expect_correction(<<~RUBY)
-      Primitive.object_kind_of?(a, String)
+      Primitive.is_a?(a, String)
     RUBY
   end
 
   it 'registers an offense when using `Module#===` and expression' do
     expect_offense(<<~RUBY)
       String === (a || b)
-      ^^^^^^^^^^^^^^^^^^^ TruffleRuby/ReplaceWithPrimitiveObjectKindOf: Use `Primitive.object_kind_of?` instead of `#kind_of?` or `#is_a?`
+      ^^^^^^^^^^^^^^^^^^^ TruffleRuby/ReplaceWithPrimitiveObjectKindOf: Use `Primitive.is_a?` instead of `#kind_of?` or `#is_a?`
     RUBY
 
     expect_correction(<<~RUBY)
-      Primitive.object_kind_of?((a || b), String)
+      Primitive.is_a?((a || b), String)
     RUBY
   end
 
   it 'registers an offense when there is no explicit receiver of `#kind_of?`' do
     expect_offense(<<~RUBY)
       kind_of?(String)
-      ^^^^^^^^^^^^^^^^ TruffleRuby/ReplaceWithPrimitiveObjectKindOf: Use `Primitive.object_kind_of?` instead of `#kind_of?` or `#is_a?`
+      ^^^^^^^^^^^^^^^^ TruffleRuby/ReplaceWithPrimitiveObjectKindOf: Use `Primitive.is_a?` instead of `#kind_of?` or `#is_a?`
     RUBY
 
     expect_correction(<<~RUBY)
-      Primitive.object_kind_of?(self, String)
+      Primitive.is_a?(self, String)
     RUBY
   end
 
   it 'registers an offense when there is no explicit receiver of `#is_a?`' do
     expect_offense(<<~RUBY)
       is_a?(String)
-      ^^^^^^^^^^^^^ TruffleRuby/ReplaceWithPrimitiveObjectKindOf: Use `Primitive.object_kind_of?` instead of `#kind_of?` or `#is_a?`
+      ^^^^^^^^^^^^^ TruffleRuby/ReplaceWithPrimitiveObjectKindOf: Use `Primitive.is_a?` instead of `#kind_of?` or `#is_a?`
     RUBY
 
     expect_correction(<<~RUBY)
-      Primitive.object_kind_of?(self, String)
+      Primitive.is_a?(self, String)
     RUBY
   end
 
